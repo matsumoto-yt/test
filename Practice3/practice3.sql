e8ff4fcc7df91a25b8b5237afea57e03e9ca794f
matsumoto-yt <matsumoto.work99@gmail.com>
2022-06-11 20:51:16 +0900

    6月11日の更新

---
+++ Practice3/practice3.sql
@@ -0,0 +1,17 @@
+drop detabase if exists practice03db;
+create database practice03db;
+
+use practice03db;
+
+create table test_table(
+
+	user_id int,
+	user_name varchar(255),
+	password varchar(255)
+
+
+);
+
+insert into test_table values(1,"taro","123");
+insert into test_table values(2,"jiro","456");
+insert into test_table values(3,"hanako","789");
